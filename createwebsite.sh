33d6c4667b43c7bd341f1c65fd24704333b7735e
Tracker-Friendly <jliwin98@danwin1210.de>
2024-03-15 19:32:54 +0000

    Fixed homeservers lol

--- createwebsite.sh
+++ createwebsite.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+rm -rf website/*
+rm -rf burgernotes-client-web
 git clone https://centrifuge.hectabit.org/hectabit/burgernotes-client-web.git --depth=1
 mv burgernotes-client-web/* website/
-rm -r burgernotes-client-web website/index.html website/README.md website/LICENSE
+rm -rf burgernotes-client-web website/index.html website/README.md website/LICENSE
