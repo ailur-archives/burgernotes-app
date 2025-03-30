b8355808ffab3356066e4204cffe3995e9daa3b8
Arzumify <jliwin98@danwin1210.de>
2025-03-30 00:53:56 +0000

    Seriously flathub?

--- createwebsite.sh
+++ createwebsite.sh
@@ -2,7 +2,7 @@
 rm -rf website/*
 mkdir -p website
 rm -rf burgernotes-client-web
-git clone https://centrifuge.hectabit.org/hectabit/burgernotes-client-web.git --depth=1
+git clone https://github.com/ailur-archives/burgernotes-web --depth=1
 mv burgernotes-client-web/* website/
 cp rdir.html website/index.html
 rm -rf burgernotes-client-web website/README.md website/LICENSE.md
